ec3bafe232bfb16967fa31f3fe0b1234a3bd3dc4
carolinappreis <carolina.reis@wolfson.ox.ac.uk>
2020-01-14 15:48:36 +0000

    ...

--- Tremor/checked_codes.m
+++ Tremor/checked_codes.m
@@ -4,21 +4,22 @@ cd('C:\Users\creis\Documents\GitHub\CR_script\Tremor')
 cd('/Users/Carolina/Documents/GitHub/CR_script/Tremor')
 
 
-run('Periph_PhasicStim_smr.m')
+run('Periph_PhasicStim_smr.m') 
 
-run('peripheraltremor_newcode.m')
+run('peripheraltremor_newcode.m') %%%%BA Response curves
 
 run('NS_sigmoid.m')
 
 run('pls_sig.m')% runs 'code_before_PLScell.m' 
 
-run('NS_new')
+run('NS_new') %%%%BA baseline do non stim 
 
 run('rc_axis_ns_thresh')
 
 run('median_split_a')
 
-run('all_plots') % arcs from rc_axis_ns_threshold; thresholds from NS_new; median split from medial_split_a
+run('all_plots') 
+% arcs from rc_axis_ns_threshold; thresholds from NS_new; median split from medial_split_a
 
 run('plot_pls_ns') % absolute amplitude bef posture, before stim, end stim for ns and pls - data from NS_sigmoid and pls_sig
     run('plot_pls_ns_demo') % diagram of envelope
